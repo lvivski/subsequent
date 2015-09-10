1d203fee76feef3a3f4f3a73dc0a3885709c134a
Yehor Lvivski <lvivski@gmail.com>
2015-09-10 18:36:22 +0300

    1.0.1

--- subsequent.js
+++ subsequent.js
@@ -23,34 +23,55 @@
       tick = false;
     }
     if (typeof setImmediate === "function") {
-      nextTick = function(fn) {
+      return function(fn) {
         enqueue(fn) && setImmediate(execute);
       };
-    } else if (typeof process === "object" && process.nextTick) {
-      nextTick = function(fn) {
+    }
+    if (typeof process === "object" && process.nextTick) {
+      return function(fn) {
         enqueue(fn) && process.nextTick(execute);
       };
-    } else if (root.postMessage) {
-      var message = "__subsequent", onMessage = function(e) {
-        if (e.data === message) {
-          e.stopPropagation && e.stopPropagation();
-          execute();
-        }
-      };
-      if (root.addEventListener) {
-        root.addEventListener("message", onMessage, true);
-      } else {
-        root.attachEvent("onmessage", onMessage);
-      }
-      nextTick = function(fn) {
-        enqueue(fn) && root.postMessage(message, "*");
-      };
-    } else {
-      nextTick = function(fn) {
-        enqueue(fn) && setTimeout(execute, 0);
+    }
+    var MutationObserver = root.MutationObserver || root.WebKitMutationObserver;
+    if (MutationObserver) {
+      var val = 1, node = document.createTextNode("");
+      new MutationObserver(execute).observe(node, {
+        characterData: true
+      });
+      return function(fn) {
+        enqueue(fn) && (node.data = val *= -1);
       };
     }
-    return nextTick;
+    if (root.postMessage) {
+      var isPostMessageAsync = true;
+      if (root.attachEvent) {
+        var checkAsync = function() {
+          isPostMessageAsync = false;
+        };
+        root.attachEvent("onmessage", checkAsync);
+        root.postMessage("__check", "*");
+        root.detachEvent("onmessage", checkAsync);
+      }
+      if (isPostMessageAsync) {
+        var message = "__subsequent", onMessage = function(e) {
+          if (e.data === message) {
+            e.stopPropagation && e.stopPropagation();
+            execute();
+          }
+        };
+        if (root.addEventListener) {
+          root.addEventListener("message", onMessage, true);
+        } else {
+          root.attachEvent("onmessage", onMessage);
+        }
+        return function(fn) {
+          enqueue(fn) && root.postMessage(message, "*");
+        };
+      }
+    }
+    return function(fn) {
+      enqueue(fn) && setTimeout(execute, 0);
+    };
   }();
   if (typeof define === "function" && define.amd) {
     define(function() {
